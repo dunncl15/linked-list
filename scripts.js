7dad65c0852cffc46bd5e0a835393052910ff598
Daniel Bucket <danielludwick@gmail.com>
2016-12-11 16:41:20 -0700

    add function for displaying user link

--- scripts.js
+++ scripts.js
@@ -2,12 +2,14 @@ var userWebsiteTitle = document.querySelector('.web-title');
 var userWebsiteURL = document.querySelector('.web-url');
 var enterButton = document.querySelector('.enter-btn');
 var newDiv;
+var newTitle;
 var userEntry = [];
+var newLink;
 
 //Enter Button click event//
 enterButton.addEventListener('click', function(){
     getUserInput();
-    addBookMark();
+    // addBookMark();
     console.log('addbookmark');
 });
 
@@ -22,3 +24,27 @@ function addDiv() {
   newDiv.classList.add('bookmark');
   document.body.appendChild(newDiv);
 }
+
+function addTitle () {
+  newTitle = document.createElement('p');
+  newTitle.classList.add('website-title');
+  var userTitle = document.createTextNode(userEntry[0]);
+  newDiv.appendChild(newTitle);
+}
+
+function addLink () {
+  newLink = document.createElement('a');
+  newLink.classList.add('website-url');
+  var userLink = document.createTextNode(userEntry[1]);
+  newDiv.appendChild(newLink);
+}
+
+
+
+//Use this nwMark() for populating new bookmark box
+// function newMark(webName, urlLocate, read, dlt) {
+//   this.webName = ;
+//   this.urlLocate = ;
+//   this.read = ;
+//   this.dlt = ;
+// }
