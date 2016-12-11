1c869950daec3d05d29e0262b5cf2a3378fe1757
Daniel Bucket <danielludwick@gmail.com>
2016-12-11 15:58:53 -0700

    finally! a start to creating new bookmark

--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
-var userWebsiteTitle = document.querySelector('.web-title')
-var userWebsiteURL = document.querySelector('.web-url')
-var enterButton = document.querySelector('.enter-btn')
-
+var userWebsiteTitle = document.querySelector('.web-title');
+var userWebsiteURL = document.querySelector('.web-url');
+var enterButton = document.querySelector('.enter-btn');
+var newDiv;
+var userEntry = [];
 
 //Enter Button click event//
 enterButton.addEventListener('click', function(){
-
-  console.log();
+    getUserInput();
+    addBookMark();
+    console.log('addbookmark');
 });
 
-function getWebEntry() {
+function getUserInput() {
   var userTitleInput = userWebsiteTitle.value;
   var userURLInput = userWebsiteURL.value;
-  console.log(userTitleInput, userURLInput);
+  userEntry = [userTitleInput, userURLInput];
+};
+
+function addDiv() {
+  newDiv = document.createElement('div');
+  newDiv.classList.add('bookmark');
+  document.body.appendChild(newDiv);
 }
