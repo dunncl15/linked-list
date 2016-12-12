e274d71a92c613868b821cfc07580b9de9054da8
Daniel Bucket <danielludwick@gmail.com>
2016-12-12 15:38:53 -0700

    functional box clone with population

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ var enterButton = document.querySelector('.enter-btn');
 var websiteTitle = document.querySelector('.websiteTitle');
 var websiteURL = document.querySelector('.websiteURL');
 
-//Enter Button click event//
 enterButton.addEventListener('click', function(){
   var userTitleInputValue = userTitleInput.value;
   var userURLInputValue = userURLInput.value;
@@ -26,6 +25,3 @@ function newBox() {
   var section1 = document.getElementsByTagName('section')[1];
   section1.appendChild(clone);
 }
-
-//can I populate the form before cloning it?
-//YES! You Can!
